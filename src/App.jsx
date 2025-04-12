bd66e2729ab352afa1fdc245764a9532b653f792
ayu-bhar <ayubhar870@gmail.com>
2025-04-12 18:48:35 +0530

    saving changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,95 @@
-import { useState } from 'react'
+import { use, useState } from 'react'
 import './App.css'
+import { useEffect } from 'react'
+import { createBrowserRouter , RouterProvider } from 'react-router-dom'
+import Manager from './components/Manager'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import MobileNavBar from './components/MobileNavBar'
+import Box from './components/box'
+import Profile from './pages/Profile'
+import Ambu from './pages/Ambu'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+import ProtectedRoute from './components/ProtectedRoute'
+import Care from './pages/Care'
+import Mainhospital from './pages/Mainhospital'
+
 
 function App() {
+  // Initialize isAuthenticated from localStorage
+  const [isAuthenticated, setIsAuthenticated] = useState(() => {
+    const storedAuthState = localStorage.getItem('isAuthenticated');
+    return storedAuthState === 'true'; // Convert string to boolean
+  });
+
+  const [user , setUser] = useState(()=>{
+    const store = localStorage.getItem('user');
+    return store;
+  });
+  // Save authentication state to localStorage whenever it changes
+  useEffect(() => {
+    localStorage.setItem('isAuthenticated', isAuthenticated);
+    console.log("Saving isAuthenticated to localStorage:", isAuthenticated);
+  }, [isAuthenticated]);
+  const router = createBrowserRouter([
+    {
+    path: '/', 
+    element: <><Navbar/><Box/><MobileNavBar/><Footer/></>,
+    errorElement: <h1>404</h1>
+  },
+  {
+    path: '/profile',
+    element: (
+      <ProtectedRoute isAuthenticated={isAuthenticated}>
+        <Navbar />
+        <MobileNavBar />
+        <Profile />
+        <Footer />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path : '/ambulance',
+    element : <><Ambu/></>
+  },
+  {
+    path:'/carecompass',
+    element : <><Care/></>
+  },
+  {
+    path : '/signup',
+    element : <><Signup/><Footer/></>,
+  },
+  {
+    path : '/login',
+    element : <><Login
+    isAuthenticated={isAuthenticated}
+    setIsAuthenticated={setIsAuthenticated}
+  /><Footer/></>
+  }
+  ,
+  {
+    path : '/hospital',
+    element : <><Mainhospital/></>,
+
+  }
+  ]);
+  // const rout = createBrowserRouter([{
+  //   path: '/',
+  //   element: <><Footer/></>,
+  //   errorElement: <h1>404</h1>
+  // }])
 
   return (
-    <>
-    
-    </>
+    <div className='relative min-h-[100vh]' >
+    <Manager/>
+    <RouterProvider router={router} />
+    <div className="main-foot">
+      {/* <RouterProvider router={rout} /> */}
+    </div>
+      
+    </div> 
   )
 }
 
---
+++ src/components/Manager.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Manager() {
+  return (
+    <div>
+       <div className="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#22c55e_100%)]"></div>
+    </div>
+  )
+}
+
+export default Manager
