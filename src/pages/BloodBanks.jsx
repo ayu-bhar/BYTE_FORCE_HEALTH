7c24ecc696d5a1d886860d3a03935d00478245ab
kartikey4000 <kartik242006@gamil.com>
2025-04-12 23:44:14 +0530

    bloodbank

--- src/pages/BloodBanks.jsx
+++ src/pages/BloodBanks.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
-
-const BloodBanks = () => {
+import './hospital.css'
+const Bloodbank = ({data, imgUrl})=> {
   return (
-    <div>
-       
+    <div className='container'>
+      <img src={imgUrl} alt='bloodbank' />
+      <div className='bloodbankDashboard'>
+      <div className='BloodbankName'>
+        <div className="placeHolder">Click here</div>
+          {data.website ? (
+            <a
+              href={data.website}
+              target='_blank'
+              rel='noopener noreferrer'
+              className='bloodbank-link'
+            >
+              {data.name}
+            </a>
+          ) : (
+            <span>{data.name}</span>
+          )}
+        </div>
+        <div className='Address'>{data.vicinity}</div>
+        <div className='active-status'>
+          {data.opening_hours?.open_now ? 'Open Now' : 'Closed'}
+        </div>
+      </div>
     </div>
   )
 }
 
-export default BloodBanks
+export default Bloodbank
