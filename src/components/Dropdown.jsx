ea01fc8ded6868992e0c857b7fabdba869f31c22
Harsh6542 <chandraharsh094@gmail.com>
2025-04-12 19:30:34 +0530

    box dropdown care

---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,40 @@
+import React,{useState} from "react";
+
+const Dropdown = ()=>{
+         
+         const[isOpen,setisOpen] = useState(false)
+         const handleMouseEnter = () => setisOpen(true);
+         const handleMouseLeave = () => setisOpen(false);
+
+         return(
+              <div className="dropdown"
+                onMouseEnter = {handleMouseEnter}
+                onMouseLeave = {handleMouseLeave}
+                style = {{position:"relative", display:"inline-block"}}>
+              
+              <lord-icon  className = "dropdown"
+                            src="https://cdn.lordicon.com/nnzfcuqw.json"
+                            trigger="hover"
+                            stroke="bold"
+                            colors="primary:white,secondary:#ee6d66">
+                        </lord-icon>{
+                             isOpen && (
+                                <ul 
+                                  className="dmenu"
+                                  style={{
+                                    position:"absolute",
+                                    zIndex:1000,
+                                    border:"2px solid grey",borderRadius:"10px",boxShadow:"2px 2px 2px grey"
+                                  }}>
+                                    <li style={{borderBottom: "2px solid grey" }}>+919259455951</li>
+                                    <li>abc.hark@gmail.com</li>
+                                    
+                                  </ul>
+                             )
+                        }
+              </div>
+         )
+}
+
+export default Dropdown
+
---
+++ src/pages/Care.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+function Care() {
+    return (
+
+        <div>
+            <Navbar />
+            <div>
+                <h1 style={{ fontSize: '40px', textAlign: 'center', marginBottom: '20px', boxShadow: '2px 2px 2px grey', textShadow: '3px 3px 3px grey' }}><b>CareCompass</b></h1>
+
+                At Health Buddies, we believe that knowledge is power when it comes to health and well-being. Our awareness programs are designed to educate, engage, and empower communities to take proactive steps towards healthier lives.
+
+
+                <li><b>Emergency Preparedness:</b> Guidance on handling medical emergencies and knowing when and how to seek help.</li>
+                <li><b>Basic First Aid Tips:</b> Life-saving tips for immediate care during emergencies, such as treating cuts, burns, and performing CPR.   </li>
+                <li><b>Promoting Regular Health Checkups:</b> Encouraging preventive measures to identify and address health issues early.   </li>
+                <li><b>Healthy Lifestyle Choices:</b> Tips for nutrition, exercise, mental health, and overall well-being.   </li>
+                <li><b>Basic Hygiene Practices:</b> Simple yet effective tips to maintain hygiene, such as frequent handwashing, sanitizing surfaces, and proper waste disposal.   </li>
+                <li><b>Daily Hygiene Habits:</b> Incorporating cleanliness into everyday routines:   </li>
+
+                <li><b>Morning Rituals:</b>Brush your teeth at least twice a day, wash your face, and take a refreshing shower. </li>
+                <li><b>Hand Hygiene:</b> Wash your hands before eating, after using the restroom, or touching dirty surfaces.  </li>
+                <li><b>Clean Clothes:</b> Wear fresh, clean clothes daily to prevent the buildup of germs.   </li>
+                <li><b>Hydration and Nutrition: </b>Keep water bottles clean and store food properly to maintain freshness. </li>
+                <li><b>Personal Belongings:</b> Regularly sanitize items like phones, wallets, and other frequently-used accessories.   </li>
+                <li style={{ marginBottom: '10px' }}><b>Before Bedtime:</b> Wash your face to remove dirt and pollutants, and ensure your sleeping environment is clean and comfortable. </li>
+
+                <h1 style={{ fontSize: '20px' }}><b>Some Basic First Aid tips</b></h1>
+                <li>Perform the 3 Cs of first aid before doing any care work—check your surroundings for safety, call for help, and then care for the person.</li>
+                <li>Check if an unconscious person is responsive or breathing. Give rescue breaths and chest compressions if they’re unresponsive but have a pulse.</li>
+                <li>Wear gloves to protect yourself from bloodborne pathogens while treating cuts, scrapes, minor burns, or other injuries.</li>
+                <li>Remain calm when providing first aid, as this will help you think clearly and act quickly.</li>
+                <li>Assess the situation and determine the person’s needs. </li>
+                <li>Call for help.</li>
+                <li>Stop the bleeding.</li>
+                <li>Stay with the person</li>
+                <li>Maintain body heat</li>
+                <li>Clean wounds</li>
+                <h1 style={{ fontSize: '20px' }}> <b>Our Mission:</b>  </h1>
+                To foster a culture of health awareness that empowers individuals to make informed decisions for themselves and their loved ones. Together, we aim to create a community that prioritizes prevention, preparedness, and good health practices every single day.
+
+                Join us in spreading awareness, saving lives, and building a healthier future for all!
+
+            </div>
+            <br />
+            <Footer />
+        </div>
+
+    )
+}
+
+export default Care 
